e7aa638113357b231859b87acb774de42049fbf6
valariembithe <valmbithe691@gmail.com>
2024-02-13 13:05:51 +0300

    Unauthorized task

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> None:
+    '''Error handler 401'''
+    abort(401)
+    
