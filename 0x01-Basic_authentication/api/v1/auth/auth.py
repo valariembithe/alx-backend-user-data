5ef634beb8deabd7ed91cf1be09e41884797f826
valariembithe <valmbithe691@gmail.com>
2024-02-13 13:30:53 +0300

    task

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from flask import request
+
+class Auth:
+    ''' Authorization '''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        ''' Checks if a path requires authentication '''
+        if path is not None and excluded_paths is not None:
+            return False
+        return True
+    
+    def authorization_header(self, request=None) -> str:
+        '''Gets the authorization header field from the request'''
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        ''''Validates user'''
+        return None
