abdf7ba92f6655be81b442495c42752a9a1ca55d
valariembithe <valmbithe691@gmail.com>
2024-02-21 00:43:01 -0800

    Task

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import bcrypt
+from uuid import uuid4
+from typing import Union
+from sqlalchemy.orm.exc import NoResultFound
+
+from db import DB
+from user import User
+
+
+def _hash_password(password: str) -> bytes:
+    ''' Converts password to bytes '''
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        ''' Register a new user '''
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError('User {} already exists'.format(email))
