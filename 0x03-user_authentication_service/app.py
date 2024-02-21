7bb519f3ec4041737f2fd94574301b4721baf125
valariembithe <valmbithe691@gmail.com>
2024-02-21 01:40:20 -0800

    App

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -10,9 +10,10 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def index():
+def index() -> str:
     '''Index page '''
     return jsonify({"message": "Bienvenue"})
+
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users() -> str:
@@ -23,6 +24,7 @@ def users() -> str:
         return jsonify({'email': email, "message": "User created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
