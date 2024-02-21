edd03392f51bb5cfbafdafe92ea011e9bc9c359f
valariembithe <valmbithe691@gmail.com>
2024-02-21 01:54:07 -0800

    Task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""A module for authentication-related routines.
+"""
 import bcrypt
 from uuid import uuid4
 from typing import Union
@@ -9,8 +11,15 @@ from user import User
 
 
 def _hash_password(password: str) -> bytes:
-    ''' Converts password to bytes '''
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    """Hashes a password.
+    """
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Generates a UUID.
+    """
+    return str(uuid4())
 
 
 class Auth:
@@ -18,23 +27,30 @@ class Auth:
     """
 
     def __init__(self):
+        """Initializes a new Auth instance.
+        """
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        ''' Register a new user '''
+        """Adds a new user to the database.
+        """
         try:
             self._db.find_user_by(email=email)
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
-        raise ValueError('User {} already exists'.format(email))
+        raise ValueError("User {} already exists".format(email))
 
-    def valid_login(email: str, password: str) -> bool:
-        ''' Check if the user is valid '''
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks if a user's login details are valid.
+        """
         user = None
         try:
             user = self._db.find_user_by(email=email)
             if user is not None:
-                return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+                return bcrypt.checkpw(
+                    password.encode("utf-8"),
+                    user.hashed_password,
+                )
         except NoResultFound:
             return False
         return False
