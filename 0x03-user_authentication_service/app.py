b590acb9f4d8b84789168ca0e17a277c72b41f26
valariembithe <valmbithe691@gmail.com>
2024-02-21 01:34:10 -0800

    App

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -11,6 +11,15 @@ def index():
     '''Index page '''
     return jsonify({"message": "Bienvenue"})
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    ''' Creates an account '''
+    email, password = request.form.get("email"), request.form.get('password')
+    try:
+        Auth.register_user(email, password)
+        return jsonify({'email': email, "message": "User created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
